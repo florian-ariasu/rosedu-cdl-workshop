530b4bd612c844378a6e94e83ecc63636b98a2d7
Razvan Deaconescu <razvan@rosedu.org>
2024-10-12 08:57:45 +0300

    Add clean up scripts
    These scripts are to be used in case anything bad happened to the
    repository.
    
    Signed-off-by: Razvan Deaconescu <razvan@rosedu.org>

---
+++ reset-all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+reset_branch_to_remote()
+{
+    git checkout "$1"
+    git reset --hard "$2"/"$1"
+}
+
+./clean-all.sh
+
+git fetch origin
+reset_branch_to_remote test origin
+reset_branch_to_remote scripts origin
+reset_branch_to_remote base origin
+reset_branch_to_remote main origin
